2aeb6ac1b6cea6baa142b6b6d4d4a581972321ca
Lokesh S <79404752+Lokie-codes@users.noreply.github.com>
2022-12-31 13:15:27 +0000

    Co-authored-by: Samarth H Chinivar <SamarthHChinivar@users.noreply.github.com>

---
+++ rotaract-club/src/LandingPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Rotaract Club of SVIT </h1>
+      <p>We are a group of dedicated students who are committed to making a difference in our community through service and leadership.</p>
+      <Form>
+        <Form.Group controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter name" />
+        </Form.Group>
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group controlId="formMessage">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows="3" />
+        </Form.Group>
+        <Form.Group controlId="formSubmit">
+          <Form.Control type="submit" value="Submit" />
+        </Form.Group>
+      </Form>
+    </div>
+  );
+};
+
+export default LandingPage;
